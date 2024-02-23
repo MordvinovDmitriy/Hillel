6340cf3d25ef9226db4f43c5181f9a676af4d66c
MordvinovDmitriy <kosmonavtua@gmial.com>
2024-02-23 17:38:07 +0200

    first commit

--- home_work/homework_14/14_2.py
+++ home_work/homework_14/14_2.py
@@ -6,19 +6,20 @@ def recogn_num(num):
     sep_pos = num.find('.')
     if num == '0':
         ret_str = 'Ви ввели нуль'
-    elif num[0] == '-' and num.count('.') <= 1:
-        if num[1:].isdigit():
-            ret_str = f'Ви ввели від\'ємне ціле число: {int(num)}'
-        elif num[1] == '.' and num[2:].isdigit():
-            ret_str = f'Ви ввели від\'ємне дробове число: {float("-0" + num[1:])}'
-        elif num[1:sep_pos].isdigit() and num[sep_pos + 1:].isdigit():
-            ret_str = f'Ви ввели від\'ємне дробове число: {float(num)}'
-    elif num.isdigit():
-        ret_str = f'Ви ввели позитивне ціле число: {int(num)}'
-    elif num.count('.') == 1 and num[:sep_pos].isdigit() and num[sep_pos + 1:].isdigit():
-        ret_str = f'Ви ввели позитивне дробове число: {float(num)}'
-    elif num[0] == '.' and num[1:].isdigit():
-        ret_str = f'Ви ввели позитивне дробове число: {float("0"+num)}'
+    elif len(num) > 0:
+        if num[0] == '-' and num.count('.') <= 1:
+            if num[1:].isdigit():
+                ret_str = f'Ви ввели від\'ємне ціле число: {int(num)}'
+            elif num[1] == '.' and num[2:].isdigit() and len(num) > 2:
+                ret_str = f'Ви ввели від\'ємне дробове число: {float("-0" + num[1:])}'
+            elif num[1:sep_pos].isdigit() and num[sep_pos + 1:].isdigit():
+                ret_str = f'Ви ввели від\'ємне дробове число: {float(num)}'
+        elif num.isdigit():
+            ret_str = f'Ви ввели позитивне ціле число: {int(num)}'
+        elif num.count('.') == 1 and num[:sep_pos].isdigit() and num[sep_pos + 1:].isdigit():
+            ret_str = f'Ви ввели позитивне дробове число: {float(num)}'
+        elif num[0] == '.' and num[1:].isdigit():
+            ret_str = f'Ви ввели позитивне дробове число: {float("0"+num)}'
     return ret_str
 
 
