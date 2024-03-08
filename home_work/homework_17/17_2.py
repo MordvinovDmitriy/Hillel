3412dab7353a163ebfbaddf799a42a431ac9af1a
MordvinovDmitriy <kosmonavtua@gmial.com>
2024-03-08 05:29:39 +0200

    added: homework_17_1 base class auto 17_2 truck and auto

--- home_work/homework_17/17_2.py
+++ home_work/homework_17/17_2.py
@@ -28,22 +28,26 @@ class car(auto):
 
 truck_1 = truck('Iveco', 3, 'Daily', '1345 kg', 'white')
 truck_1.move()
+truck_1.stop()
 truck_1.birthday()
 truck_1.load()
 print(truck_1.__dict__)
 
 truck_2 = truck('Chevrolet', 17, 'Express', '1473 kg')
 truck_2.move()
+truck_2.stop()
 truck_2.birthday()
 truck_2.load()
 print(truck_2.__dict__)
 
 car_1 = car('Audi', 14, 'A4', 216, 'black')
 car_1.move()
+car_1.stop()
 car_1.birthday()
 print(car_1.__dict__)
 
 car_2 = car('Toyota', 5, 'Camry', 220, 'blue','1560 kg')
 car_2.move()
+car_2.stop()
 car_2.birthday()
 print(car_2.__dict__)
